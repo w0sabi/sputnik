9fc2c801755a45d7be87e3c03ea316f465930b08
Simon Baumann <simon@hor1zon.io>
2021-12-25 20:02:30 +0100

    Added error when no launches found

--- commands/launch.js
+++ commands/launch.js
@@ -8,9 +8,9 @@ module.exports = {
                 result = result['result'];
                 result.forEach(function myFunction(item, index) {
                     var date = new Date(result[index]['sort_date'] * 1000);
-                    var fulltime = date.toLocaleString();
+                    var fulltime = date.toLocaleString() + " MEZ";
                     if(result[index]['win_open'] === null) {
-                        result[index]['win_open'] = "Folgt"
+                        result[index]['win_open'] = "Folgt";
                         fulltime = "Folgt"
                     }
                     sys.embed(message,result[index]['name'],'Startfenster: ' + fulltime + '\nProvider: ' + result[index]['provider']['name'] + '\nRakete: ' + result[index]['vehicle']['name'],sys.config.COLORS.DEFAULT,'https://www.rocketlaunch.live/launch/' + result[index]['slug'])
@@ -19,12 +19,16 @@ module.exports = {
         } else {
             sys.getNextLaunches(3,args[0],function(result) {
                 result = result['result'];
-                result.forEach(function myFunction(item, index) {
-                    if(result[index]['win_open'] === null) {
-                        result[index]['win_open'] = "Folgt"
-                    }
-                    sys.embed(message,result[index]['name'],'Startfenster: ' + result[index]['win_open'] + '\nProvider: ' + result[index]['provider']['name'] + '\nRakete: ' + result[index]['vehicle']['name'],sys.config.COLORS.DEFAULT,'https://www.rocketlaunch.live/launch/' + result[index]['slug'])
-                });
+                if(result.length > 0) {
+                    result.forEach(function myFunction(item, index) {
+                        if(result[index]['win_open'] === null) {
+                            result[index]['win_open'] = "Folgt"
+                        }
+                        sys.embed(message,result[index]['name'],'Startfenster: ' + result[index]['win_open'] + '\nProvider: ' + result[index]['provider']['name'] + '\nRakete: ' + result[index]['vehicle']['name'],sys.config.COLORS.DEFAULT,'https://www.rocketlaunch.live/launch/' + result[index]['slug'])
+                    });
+                } else {
+                    sys.embed(message,'Keine Raketenstarts gefunden.','Wir konnte keine Ergebnisse für "' + args[0] + '" finden.',sys.config.COLORS.DANGER);
+                }
             });
         }
     },
